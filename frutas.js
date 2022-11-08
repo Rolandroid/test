c01948d68a699e8de8bfb8f8e4d284417b965d0f
Rolandroid <117363873+Rolandroid@users.noreply.github.com>
2022-11-08 09:39:17 -0300

    Frutas listo.
    listo frutas

--- frutas.js
+++ frutas.js
@@ -2,5 +2,15 @@ let frutas = [{
     nombre:"naranja",
     precio : 10,
     rico : false
-},]
+},{
+    nombre:"manzana",
+    precio : 15,
+    rico : true
+},{ 
+    nombre:"banana",
+    precio : 15,
+    rico : false
+  }    
+}]
+
 module.exports = frutas
