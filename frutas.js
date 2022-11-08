b5dcf0be349d7e5a6252eaf52b16439315ec7085
Rolandroid <r.lisarazo@gmail.com>
2022-11-08 09:31:12 -0300

    me la mande

--- frutas.js
+++ frutas.js
@@ -2,18 +2,5 @@ let frutas = [{
     nombre:"naranja",
     precio : 10,
     rico : false
-<<<<<<< HEAD
-},
-{}]
-=======
-},{
-    nombre:"manzana",
-    precio : 15,
-    rico : true
-},{ 
-    nombre:"banana",
-    precio : 15,
-    rico : false
-  }           
-    ]
->>>>>>> 5b9109e7ab6c6c0ac8d5ed128b5e4d1ef10818e4
+},]
+module.exports = frutas
